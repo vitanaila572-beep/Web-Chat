45f40b202fcdcc61761946cd11079a54c4699c19
vitanaila572-beep <vitanaila572@gmail.com>
2026-09-03 00:00:49 +0700

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// assets/js/firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+export const app = initializeApp({
+  apiKey: "AIzaSyAJ0S---xqH5UjUFyePuvHZRp8My5pXifA",
+  authDomain: "chat-c639a.firebaseapp.com",
+  projectId: "chat-c639a",
+  storageBucket: "chat-c639a.firebasestorage.app",
+  messagingSenderId: "1012375212670",
+  appId: "1:1012375212670:web:7aca174953055b1c62be2f"
+});
