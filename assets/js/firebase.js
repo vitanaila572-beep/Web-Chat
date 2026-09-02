b273157e7735f7df938b4d051d6154d8060e23d3
vitanaila572-beep <vitanaila572@gmail.com>
2026-09-03 00:38:51 +0700

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,12 +1,17 @@
-// assets/js/firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
-export const app = initializeApp({
-  apiKey: "AIzaSyAJ0S---xqH5UjUFyePuvHZRp8My5pXifA",
+const firebaseConfig = {
+  apiKey: "...",
   authDomain: "chat-c639a.firebaseapp.com",
   projectId: "chat-c639a",
   storageBucket: "chat-c639a.firebasestorage.app",
-  messagingSenderId: "1012375212670",
-  appId: "1:1012375212670:web:7aca174953055b1c62be2f"
-});
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
